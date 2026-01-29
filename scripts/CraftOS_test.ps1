9b120fa28b11c45a29b0bb9b6f6f0252e9743e9d
dav2010ID <dashkodavid2010@gmail.com>
2026-01-29 22:32:55 +0100

    refactor

--- scripts/CraftOS_test.ps1
+++
@@ -1,2 +0,0 @@
-cd C:\Users\David\Desktop\proj\lua
-CraftOS-PC_console.exe --cli --exec mount ./ C:\Users\David\Desktop\proj\lua
--- scripts/run_lua_tests.ps1
+++
@@ -1,9 +0,0 @@
-﻿param(
-  [string]$LuaExe = "lua",
-  [string]$Tests = "lua/tests/run_tests.lua"
-)
-
-& $LuaExe $Tests
-if ($LASTEXITCODE -ne 0) {
-  exit $LASTEXITCODE
-}
